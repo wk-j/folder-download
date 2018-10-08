6d05f466a76f3b515f8e4f3ef1c2d785e379f9da
wk-j <somnuk.wk@gmail.com>
2018-10-08 18:07:54 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## Download GitHub Folder
+
+Download specific GitHub folder (using SVN)
+
+[![NuGet](https://img.shields.io/nuget/v/wk.FolderDownload.svg)](https://www.nuget.org/packages/wk.FolderDownload)
+
+## Installation
+
+```bash
+brew install subversion
+dotnet tool install -g wk-folder-download
+```
+
+## Usage
+
+```bash
+wk-folder-download https://github.com/dotnet/orleans/tree/master/Samples/2.0 Sample
+```
---
+++ src/FolderDownload/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+let PS = StartProcess.Processor.StartProcess
+
+[<EntryPoint>]
+let main argv =
+    match argv with
+    | [| url; targetDir |] ->
+        // https://github.com/dotnet/orleans/tree/master/Samples/2.0
+        // https://github.com/dotnet/orleans/trunk/Sample/2.0
+        let svn = url.Replace("tree/master", "trunk")
+        let command = sprintf """svn export "%s" "%s" """ svn targetDir
+        printfn "> %s" command
+        PS command
+        0
+    | _ ->
+        printfn "> Invalid argument"
+        -1
